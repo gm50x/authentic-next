add7a85948256f52c732279d996837117b83045c
Getulio Magela Silva <getuliomagela@outlook.com>
2021-05-05 14:29:08 -0300

    feat: hiding secrets in between

--- pages/api/env.js
+++ pages/api/env.js
@@ -13,16 +13,21 @@ export default (req, res) => {
     GITHUB_CLIENT_SECRET
   } = process.env
 
+  const omitMiddleCharacters = (str, hideChar = '*', visibleCharsCount = 3) => {
+    const firstChars = str.substring(0, visibleCharsCount)
+    const lastChars = str.substring(str.length - visibleCharsCount)
+    return `${firstChars}${hideChar.repeat(str.length - visibleCharsCount * 2)}${lastChars}`
+  }
 
   res.status(200).json({
     NEXTAUTH_URL,
-    JWT_SECRET,
+    JWT_SECRET: omitMiddleCharacters(JWT_SECRET),
     AZURE_TENANT_NAME,
     AZURE_TENANT_ID,
     AZURE_CLIENT_ID,
-    AZURE_CLIENT_SECRET,
+    AZURE_CLIENT_SECRET: omitMiddleCharacters(AZURE_CLIENT_SECRET),
     AZURE_USER_FLOW,
     GITHUB_CLIENT_ID,
-    GITHUB_CLIENT_SECRET
+    GITHUB_CLIENT_SECRET: omitMiddleCharacters(GITHUB_CLIENT_SECRET)
   })
 }
